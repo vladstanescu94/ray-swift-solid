2fb80b2d5878fdcee5c313035393bea4598325a1
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-22 09:54:23 +0200

    Change return type of callback result

--- SOLID/Liskov Substitution/LiskovLoader.swift
+++ SOLID/Liskov Substitution/LiskovLoader.swift
@@ -33,11 +33,11 @@
 import Foundation
 
 protocol Loader {
-  func loadAnimals(_ callback: @escaping (Result<Any, Error>) -> Void)
+  func loadAnimals(_ callback: @escaping (Result<[String], Error>) -> Void)
 }
 
 class UserDefaultsLoader: Loader {
-  func loadAnimals(_ callback: @escaping (Result<Any, Error>) -> Void) {
+  func loadAnimals(_ callback: @escaping (Result<[String], Error>) -> Void) {
     if let savedAnimals = UserDefaults.standard.stringArray(forKey: "animals") {
       callback(.success(savedAnimals))
     } else {
@@ -47,12 +47,12 @@ class UserDefaultsLoader: Loader {
 }
 
 class NetworkLoader: Loader {
-  func loadAnimals(_ callback: @escaping (Result<Any, Error>) -> Void) {
+  func loadAnimals(_ callback: @escaping (Result<[String], Error>) -> Void) {
     let task = URLSession.shared.dataTask(with: URL(string: "https://www.animalsgalore.com/api")!) { (maybeData, _, maybeError) in
       if let _ = maybeError {
         preconditionFailure("Error occurred when downloading data")
       } else if let data = maybeData {
-        let animals = try! JSONDecoder().decode([String: String].self, from: data)
+        let animals = try! JSONDecoder().decode([String].self, from: data)
         callback(.success(animals))
       } else {
         preconditionFailure("No animals")
